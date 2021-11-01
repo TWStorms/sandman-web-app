ee1cc79667d49f1bd22a8cad97d51113d0e4cfca
officialmuneeb <muneeb.tws@gmail.com>
2021-11-01 11:52:48 +0500

    removed extras in appjs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import TopBarProgress from "react-topbar-progress-indicator";
 import "./styles/App.css";
 import Login from "./pages/login";

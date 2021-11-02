4269a0fdbcc68417dc7022efd5f3a656098d3017
officialmuneeb <muneeb.tws@gmail.com>
2021-11-02 12:38:03 +0500

    chnaged login api logic

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -12,50 +12,50 @@ import Carousel from "../../components/carousel";
 import Input from "../../components/input";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const history = useHistory();
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const history = useHistory();
 
-  const handleLogin = async () => {
-    // console.log(email, password);
-    // let item = { email, password, applicationId };
-    await fetch("https://api.sandmandoppler.bycopilot.com/v4/auth/login", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      mode: "no-cors",
-      body: JSON.stringify({
-        authenticationDetails: {
-          applicationId: "SANDMANDOPPLER",
-          email: email,
-          password: password,
-        },
-        deviceDetails: {
-          applicationVersion: "154",
-          deviceId: "12345678",
-          deviceModel: "PIXEL",
-          deviceType: "PHONE",
-          osType: "ANDROID",
-          osVersion: "9",
-          timezone: {
-            currentTimeInClientInMilliseconds: 0,
-            offsetFromUTCInMilliseconds: 0,
-            timeZoneId: "UTC",
-          },
-        },
-      }),
-    })
-      // .then((response) => response.json())
-      .then(async (json) => {
-        console.log(json);
-      });
-    // response = await response.json();
-    // console.warn(response);
-    // localStorage.setItem(JSON.stringify(response));
-    // history.push("/dashboard");
-  };
+  // const handleLogin = async () => {
+  //   // console.log(email, password);
+  //   // let item = { email, password, applicationId };
+  //   await fetch("https://api.sandmandoppler.bycopilot.com/v4/auth/login", {
+  //     method: "POST",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //     },
+  //     mode: "no-cors",
+  //     body: JSON.stringify({
+  //       authenticationDetails: {
+  //         applicationId: "SANDMANDOPPLER",
+  //         email: email,
+  //         password: password,
+  //       },
+  //       deviceDetails: {
+  //         applicationVersion: "154",
+  //         deviceId: "12345678",
+  //         deviceModel: "PIXEL",
+  //         deviceType: "PHONE",
+  //         osType: "ANDROID",
+  //         osVersion: "9",
+  //         timezone: {
+  //           currentTimeInClientInMilliseconds: 0,
+  //           offsetFromUTCInMilliseconds: 0,
+  //           timeZoneId: "UTC",
+  //         },
+  //       },
+  //     }),
+  //   })
+  //     // .then((response) => response.json())
+  //     .then(async (json) => {
+  //       console.log(json);
+  //     });
+  //   // response = await response.json();
+  //   // console.warn(response);
+  //   // localStorage.setItem(JSON.stringify(response));
+  //   // history.push("/dashboard");
+  // };
 
   // const routeChange = () => {
   //   let path = `dashboard`;
@@ -79,12 +79,12 @@ const Login = () => {
           <Input
             placeHolder="Email"
             type="text"
-            onChange={(e) => setEmail(e.target.value)}
+            // onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             placeHolder="Password"
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            // onChange={(e) => setPassword(e.target.value)}
           />
           <div class="text-right">
             <a href="#" class="text-right underline">
@@ -92,7 +92,10 @@ const Login = () => {
             </a>
           </div>
           <div class="text-center">
-            <button class="submit-btn" onClick={handleLogin}>
+            <button
+              class="submit-btn"
+              // onClick={handleLogin}
+            >
               LOG IN
             </button>
           </div>
